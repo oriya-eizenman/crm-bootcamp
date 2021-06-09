157e0f09b2d1712e31a62d05263f467079214f81
Oriya Eizenman <oriyaeizenman@Oriyas-MacBook-Pro.local>
2021-06-09 18:02:38 +0300

    update

--- 2.landing-page-vanila/scripts/test.js
+++ 2.landing-page-vanila/scripts/test.js
@@ -1,6 +1,24 @@
-var testfunction = function() {
+// var testfunction = function() {
 
-    var selector = document.querySelector('body');
-    selector.style.backgroundColor = '#'+Math.random().toString(16).substr(-6);
+//     var selector = document.querySelector('body');
+//     selector.style.backgroundColor = '#'+Math.random().toString(16).substr(-6);
 
-}
+// }
+
+$(function () {
+    $("#button").click(function () {
+        $("#button").addClass("onclic", 250, validate);
+    });
+
+    function validate() {
+        setTimeout(function () {
+            $("#button").removeClass("onclic");
+            $("#button").addClass("validate", 450, callback);
+        }, 2250);
+    }
+    function callback() {
+        setTimeout(function () {
+            $("#button").removeClass("validate");
+        }, 1250);
+    }
+});
